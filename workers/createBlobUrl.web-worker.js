ca74685d109e16499ff12c17fabc39b2320c5d71
AppVeyor <ci@appveyor.com>
2025-03-15 14:33:43 +0000

    adf2a95

---
+++ workers/createBlobUrl.web-worker.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+self.addEventListener(`message`, async event => {
+  try {
+    const {
+      url
+    } = event.data;
+    const responseFromServer = await fetch(url);
+    const fileBlob = await responseFromServer.blob();
+    const fileLink = URL.createObjectURL(fileBlob);
+    self.postMessage(fileLink);
+  } catch (err) {
+    console.log(`Не получилось загрузить файл: ${err}`);
+    return null;
+  }
+});
+/******/ })()
+;
